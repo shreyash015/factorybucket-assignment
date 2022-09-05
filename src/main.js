3e87c15966725b09b908678803175b20c51deae8
Shreyash Payasam <shreyashpayasam@Shreyashs-MacBook-Pro.local>
2022-09-05 15:56:21 +0530

    connected API

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(VueAxios, axios).mount('#app')
