9f6cec848b41257f2dd301a0a68850422ac69fa5
Shreyash Payasam <shreyashpayasam@Shreyashs-MacBook-Pro.local>
2022-09-07 15:57:21 +0530

    added props

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -5,21 +5,38 @@
       <div class="min-w-0 flex-1">
         <a href="#" class="block focus:outline-none">
           <span class="absolute inset-0" aria-hidden="true"></span>
-          <p class="truncate text-lg text-left font-medium text-gray-900">Book Title: {{bookData.docs[0].title}}</p>
-          <p class="truncate text-base text-gray-500 mt-1 text-left"> <span class="underline">Author:</span> Robin
-            Sharma</p>
+          <p class="truncate text-lg text-left font-medium text-gray-900"> <span class="underline">Book Title: </span>
+            {{bookData.title}}
+          </p>
+          <p class="truncate text-base text-gray-500 mt-1 text-left"> <span class="underline">Author:</span>
+            <span v-if="bookData.author_name">
+              {{bookData.author_name[0]}}
+            </span>
+            <span v-else>
+              N/A
+            </span>
+          </p>
         </a>
       </div>
       <p class="flex-shrink-0 whitespace-nowrap text-sm text-left md:text-right  text-gray-500">Published
-        on:{{bookData.docs[0].publish_date[1]}}</p>
+        in:
+        <span v-if="bookData.publish_date">
+          {{bookData.publish_date[0]}}
+        </span>
+        <span v-else>
+          N/A
+        </span>
+      </p>
     </div>
     <div class="mt-1">
-      <ul class="text-sm text-gray-600  text-left flex flex-col md:flex-row mt-2">
-        <span class="underline">Publishers:</span>
-        <li class="ml-1" v-for="publisher in bookData.docs[0].publisher_facet"> {{publisher}}, </li>
-      </ul>
-      <p class="text-sm mt-2 text-gray-600 line-clamp-2 text-left"> <span class="underline">Subject:</span>
-        {{bookData.docs[0].subject[0]}}</p>
+      <p class="text-sm mt-2 text-gray-600 line-clamp-2 text-left"> <span class="underline">Subject: </span>
+        <span v-if="bookData.subject">
+          {{bookData.subject[0]}}
+        </span>
+        <span v-else>
+          N/A
+        </span>
+      </p>
     </div>
   </div>
 </template>
@@ -29,48 +46,15 @@
   export default{
     data(){
       return {
-  bookData: {
-    "numFound": 8,
-    "start": 0,
-    "numFoundExact": true,
-    "docs": [
-        {
-            "title": "The 5 a.m. Club",
-            "edition_count": 3,
-            "publish_date": [
-                "2018",
-                "Jun 05, 2018"
-            ],
-            "publish_year": [
-                2018
-            ],
-            "language": [
-                "eng"
-            ],
-            "author_name": [
-                "Robin Sharma (dup. see https://openlibrary.org/works/OL221968A)"
-            ],
-            "subject": [
-                "Self-actualization (psychology)"
-            ],
-            "publisher_facet": [
-                "HarperCollins Publishers Australia",
-                "Jaico",
-                "Thorsons"
-            ],
-            "subject_facet": [
-                "Self-actualization (psychology)"
-            ],
-            "subject_key": [
-                "self-actualization_(psychology)"
-            ]
-        },
-    ],
-    "num_found": 8,
-    "q": "",
-    "offset": null
-}
       }
-    }
+    },
+  props: {
+    bookData: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
   }
 </script>
--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,24 +1,23 @@
 <template>
     <div class="">
         <div class="text-left">
-            <a class="flex cursor-pointer justify-center mb-12 md:mb-0 md:justify-start"   @click="$router.go(-1)">
-            <svg height="24" width="32" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                <polyline points="244 400 100 256 244 112"
-                    style="fill:none;stroke:white;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px" />
-                <line style="fill:none;stroke:white;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px"
-                    x1="120" x2="412" y1="256" y2="256" />
-            </svg>
-            <span>Back to search</span>
+            <a class="flex cursor-pointer justify-center mb-12 md:mb-0 md:justify-start" @click="$router.go(-1)">
+                <svg height="24" width="32" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                    <polyline points="244 400 100 256 244 112"
+                        style="fill:none;stroke:white;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px" />
+                    <line style="fill:none;stroke:white;stroke-linecap:round;stroke-linejoin:round;stroke-width:48px"
+                        x1="120" x2="412" y1="256" y2="256" />
+                </svg>
+                <span>Back to search</span>
             </a>
         </div>
         <p class="text-7xl text-white">Results</p>
-        <p class="text-4xl text-white mt-4">Total books found : {{store.booksData.numFound}}</p>
-        <p class="text-4xl text-white mt-4">Total Results displayed below : 10</p>
+        <p class="text-4xl text-white mt-4">Total books found : {{store.getBooksData.numFound}}</p>
+        <p class="text-4xl text-white mt-4">Total Results displayed below : {{getTruncatedData.length}}</p>
         <ul role="list" class="divide-y divide-gray-200  ">
             <li class="relative">
-                <ListComponent 
-                    v-for="(bookData,index) in store.booksData.docs" :key="index" 
-                    class="max-w-2xl mx-auto mt-12"/>
+                <ListComponent v-for="(bookData,index) in getTruncatedData" :key="index" class="max-w-2xl mx-auto mt-12"
+                    :bookData=bookData />
             </li>
         </ul>
     </div>
@@ -27,6 +26,7 @@
 <script>
     import ListComponent from "../components/ListComponent.vue";
     import { useStore } from '../store/index';
+    import _ from 'lodash'
 
 
     export default {
@@ -38,6 +38,11 @@
         },
         components: {
             ListComponent
+        },
+        computed:{
+            getTruncatedData(){
+               return _.take(this.store.getBooksData.docs,10);
+            }
         }
     }
 </script>
